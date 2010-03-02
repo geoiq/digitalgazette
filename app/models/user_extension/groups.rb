3a7271c3552ae9c31f89fed6cc2c50ea35134cc8
root <root@domU-12-31-39-0C-20-82.compute-1.internal>
2010-03-01 21:12:36 -0500

    Hiding last_visit_weight

--- app/models/user_extension/groups.rb
+++ app/models/user_extension/groups.rb
@@ -65,7 +65,7 @@ module UserExtension::Groups
          def most_active
            max_visit_count = find(:first, :select => 'MAX(memberships.total_visits) as id').id || 1
            select = "groups.*, " + quote_sql([MOST_ACTIVE_SELECT, 2.week.ago.to_i, 2.week.seconds.to_i, max_visit_count])
-           find(:all, :limit => 13, :select => select, :order => 'last_visit_weight + total_visits_weight DESC')
+           find(:all, :limit => 13, :select => select) #, :order => 'last_visit_weight + total_visits_weight DESC')
          end
       end
 
@@ -75,7 +75,7 @@ module UserExtension::Groups
          def most_active
            max_visit_count = find(:first, :select => 'MAX(memberships.total_visits) as id').id || 1
            select = "groups.*, " + quote_sql([MOST_ACTIVE_SELECT, 2.week.ago.to_i, 2.week.seconds.to_i, max_visit_count])
-           find(:all, :limit => 13, :select => select, :order => 'last_visit_weight + total_visits_weight DESC')
+           find(:all, :limit => 13, :select => select) #, :order => 'last_visit_weight + total_visits_weight DESC')
          end
       end
 
