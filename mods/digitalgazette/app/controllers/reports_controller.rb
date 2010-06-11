6b135f0e9f2a33164e482f369fe546923ed5bd36
root <root@frontier.geocommons.com>
2010-06-11 08:24:26 -0400

    Asset pages correctly linked in tags

--- mods/digitalgazette/app/controllers/reports_controller.rb
+++ mods/digitalgazette/app/controllers/reports_controller.rb
@@ -6,7 +6,7 @@ class ReportsController < ApplicationController
     @popular = Page.find_by_path([['limit','5'], [ 'most_viewed', "5"], ['type', 'wiki_page']])
     @recent = Page.find_by_path([ ['limit','5'], [ 'ascending', 'created_at'], ['type', 'asset_page']])
     
-    render :template => "pages/index", :locals => {:asset_type => "assets"}
+    render :template => "pages/index", :locals => {:asset_type => "AssetPage"}
   end
 
 end
