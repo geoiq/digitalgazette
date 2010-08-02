385dd3eddc2af24ef939ed0212d989f127f38e78
chris <chris@brueckenschlaeger.de>
2010-08-02 18:21:12 +0000

    working on seach results

---
+++ mods/digitalgazette/app/controllers/search_controller.rb
@@ -0,0 +1,24 @@
+class SearchController
+  
+  
+  def render_search_results
+    @path.default_sort('updated_at') if @path.search_text.empty?
+    @pages = Page.paginate_by_path(@path, options_for_me({:method => :sphinx}.merge(pagination_params))) 
+  
+    
+    # if there was a text string in the search, generate extracts for the results
+    if @path.search_text and @pages.any?
+      begin
+        add_excerpts_to_pages(@pages)
+      rescue Errno::ECONNREFUSED, Riddle::VersionError, Riddle::ResponseError => err
+        RAILS_DEFAULT_LOGGER.warn "failed to extract keywords from sphinx search: #{err}."
+      end
+    end
+
+    full_url = search_url + @path
+    handle_rss :title => full_url, :link => full_url,
+               :image => avatar_url(:id => @user.avatar_id||0, :size => 'huge')
+  end
+
+  
+end
