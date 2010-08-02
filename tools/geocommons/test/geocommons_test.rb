1f43d30f9c6c232d9fe6956d91a625589d41d23d
chris <chris@brueckenschlaeger.de>
2010-08-02 21:30:47 +0000

    unit tests for the geocommons api

--- tools/geocommons/test/geocommons_test.rb
+++ tools/geocommons/test/geocommons_test.rb
@@ -1,8 +1,25 @@
+require 'rubygems'
 require 'test/unit'
+require '../lib/geocommons'
+require 'json'
+
+GEOCOMMONS_HOST = "finder.digitalgazette.org"
+
 
 class GeocommonsTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+
+  def setup
+    @api = Geocommons::RestAPI
   end
+  
+  def test_find
+    assert_not_nil result = @api.find, ".find should return something"
+    assert_kind_of Hash, result, "The result should be a Hash"
+    assert result.keys.first == "totalResults", "the first key should be totalResults"
+    assert result.keys[1] == "itemsPerPage", "the second key should be itemsPerPage"
+    assert result.keys[2] == "entries", "the third key should be entries"
+    assert result.keys[3] == "next", "the forth key should be next"
+  end
+  
+  
 end
