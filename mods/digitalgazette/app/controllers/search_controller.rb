035258d734eb5691dc7e2c985f4efcce24cadfa4
Andi <andi@0xb5.org>
2010-08-02 23:32:29 +0200

    moved render_search_results from init script to app/controllers/search

--- mods/digitalgazette/app/controllers/search_controller.rb
+++ mods/digitalgazette/app/controllers/search_controller.rb
@@ -8,9 +8,9 @@ class SearchController < ApplicationController
   #     @popular = Page.find_by_path([['limit','5'], [ 'most_viewed', "5"], ['type', 'wiki_page']])
   #     @recent = Page.find_by_path([ ['limit','5'], [ 'ascending', 'created_at'], ['type', 'asset_page']])
 
-  
+
   SEARCHABLE_PAGE_TYPES = ["WikiPage","AssetPage","MapPage"].freeze
-  
+
   # TODO: check if there is a less hacky way / if this way is sufficient
   # GET /search
   def index
@@ -24,15 +24,20 @@ class SearchController < ApplicationController
       render_search_results
     end
   end
-    
+
+
   def render_search_results
     @path.default_sort('updated_at') if @path.search_text.empty?
+
     # if no explicit page type is already set, we limit page types to those we actually want to search in
-    merge_default_path    
+    merge_default_path
     @pages = Page.paginate_by_path(@path, options_for_me({:method => :sphinx}.merge(pagination_params)))
-    split_by_page_types  
+
+    # split results by page types
+    split_by_page_types
+    # fetch overlays from geocommons
     get_external_results
-    
+
     # if there was a text string in the search, generate extracts for the results
     if @path.search_text and @pages.any?
       begin
@@ -43,25 +48,25 @@ class SearchController < ApplicationController
     end
 
     full_url = search_url + @path
-    handle_rss :title => full_url, :link => full_url,
-               :image => avatar_url(:id => @user.avatar_id||0, :size => 'huge')
+    handle_rss(:title => full_url, :link => full_url,
+               :image => (@user ? avatar_url(:id => @user.avatar_id||0, :size => 'huge') : nil))
   end
 
-  
+
   # mix in instance variables from the external api
   def get_external_results
     if @tags
       @overlays = Geocommons::RestAPI::Overlay.paginate_by_tag(*@tags)
     else
       @overlays = Geocommons::RestAPI::Overlay.paginate(:query => @path.arg_for("text"))
-    end  
+    end
   end
-  
+
   # separate by page types into instance variables
-  def split_by_page_type
-    @pages = @pages.group_by{ |page| page.class.name}
+  def split_by_page_types
+    @grouped_pages = @pages.group_by{ |page| page.class.name}
     SEARCHABLE_PAGE_TYPES.each do |t|
-      instance_variable_set(:"@#{t.downcase.pluralize}",pages[t])
+      instance_variable_set(:"@#{t.downcase.pluralize}",@grouped_pages[t])
     end
   end
 
@@ -71,8 +76,8 @@ class SearchController < ApplicationController
     merge_path = (["type"] << SEARCHABLE_PAGE_TYPES.map(&:underscore).join("/or/type")).flatten.join('/')
     @path.merge!("type/asset/or/type/wiki/or/type/map") unless @path.first_arg_for("type")
   end
-  
-  
+
+
   # add to the path
   def prefix_path
     if params[:page_type]
--- mods/digitalgazette/init.rb
+++ mods/digitalgazette/init.rb
@@ -53,27 +53,27 @@ Dispatcher.to_prepare do
       def self.included(base)
         base.instance_eval do
           skip_before_filter :login_required, :fetch_user, :login_with_http_auth
-          alias_method_chain :render_search_results, :digitalgazette
+          # alias_method_chain :render_search_results, :digitalgazette
         end
       end
 
-      def render_search_results_with_digitalgazette
-        @path.default_sort('updated_at') if @path.search_text.empty?
-        @pages = Page.paginate_by_path(@path, options_for_me({:method => :sphinx}.merge(pagination_params)))
+      # def render_search_results_with_digitalgazette
+      #   @path.default_sort('updated_at') if @path.search_text.empty?
+      #   @pages = Page.paginate_by_path(@path, options_for_me({:method => :sphinx}.merge(pagination_params)))
 
-        # if there was a text string in the search, generate extracts for the results
-        if @path.search_text and @pages.any?
-          begin
-            add_excerpts_to_pages(@pages)
-          rescue Errno::ECONNREFUSED, Riddle::VersionError, Riddle::ResponseError => err
-            RAILS_DEFAULT_LOGGER.warn "failed to extract keywords from sphinx search: #{err}."
-          end
-        end
+      #   # if there was a text string in the search, generate extracts for the results
+      #   if @path.search_text and @pages.any?
+      #     begin
+      #       add_excerpts_to_pages(@pages)
+      #     rescue Errno::ECONNREFUSED, Riddle::VersionError, Riddle::ResponseError => err
+      #       RAILS_DEFAULT_LOGGER.warn "failed to extract keywords from sphinx search: #{err}."
+      #     end
+      #   end
 
-        full_url = search_url + @path
-        handle_rss(:title => full_url, :link => full_url,
-                   :image => (@user ? avatar_url(:id => @user.avatar_id||0, :size => 'huge') : nil))
-      end
+      #   full_url = search_url + @path
+      #   handle_rss(:title => full_url, :link => full_url,
+      #              :image => (@user ? avatar_url(:id => @user.avatar_id||0, :size => 'huge') : nil))
+      # end
     end
 
     SearchController.send(:include, SearchControllerExtension)
