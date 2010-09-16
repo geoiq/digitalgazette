d87015187b227cd8055f0f1334d2e1187788a88c
niklas <niklas@brueckenschlaeger.de>
2010-09-16 03:29:46 +0200

    attempt to add map to unified search

--- tools/geocommons/lib/geocommons/map.rb
+++ tools/geocommons/lib/geocommons/map.rb
@@ -4,6 +4,27 @@ class Geocommons::Map < Geocommons::BasePage
 
   attributes :short_classification, :author, :title, :id, :tags, :pk, :type, :description, :permissions, :link, :bbox, :created
 
+  # TODO move this to geocommons.yml
+  Crabgrass::ExternalAPI.register('map',
+                                  { :model => self.class.name,
+                                    :methods =>
+                                    { :find => "paginate"},
+                                    :query_builder => {
+                                      :keywords => {
+                                        "text" => "",
+                                        "tag" => "tag"
+                                      },
+                                      :argument_separator => " ",
+                                      :key_value_separator => ":"
+                                    }
+                                  }
+                                )
+
+  def id
+    # GeoCommons formats id as Map:123
+    @id.kind_of?(String) ? @id.split(':').last.to_i : @id
+  end
+
   def author
     @author || { }
   end
