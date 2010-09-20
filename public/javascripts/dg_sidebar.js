f3d0867626b3e2c59fc539e29352e8630666dc0d
niklas <niklas@brueckenschlaeger.de>
2010-09-20 21:54:51 +0200

    fixed sidebar (it used to add a scroll bar to the bottom. changed my strategy so it doesn't happen.)

--- public/javascripts/dg_sidebar.js
+++ public/javascripts/dg_sidebar.js
@@ -5,33 +5,42 @@ var DigitalGazette = {
             width: 16, // px
             setup: function() {
                 this.div.setAttribute('id', 'dg_sidebar_toggle');
-                 this.div.style['position'] = 'absolute';
-                 this.div.style['top'] = '0';
-                 this.div.style['left'] = '0';
-                 this.div.style['height'] = '100%';
-                 this.div.style['width'] = this.width + 'px';
-                 this.div.style['background'] = 'red';
-                 this.div.onclick = function() { DigitalGazette.Sidebar.toggle(); };
+                this.div.style['position'] = 'absolute';
+                this.div.style['top'] = '0';
+                this.div.style['left'] = '0';
+                this.div.style['height'] = '100%';
+                this.div.style['width'] = this.width + 'px';
+                this.div.style['background'] = 'red'; // TODO: remove this.
+                this.div.onclick = function() { DigitalGazette.Sidebar.toggle(); };
             },
         },
         div: document.createElement('div'),
+        wrapper: document.createElement('div'),
         setup: function() {
+            Element.extend(this.wrapper);
             Element.extend(this.div);
-            this.div.setAttribute('id', 'dg_sidebar');
-             this.div.style['position'] = 'absolute';
-             this.div.style['top'] = '160px';
-             this.div.style['right'] = '0';
-             this.div.style['background'] = 'blue';
+            this.wrapper.setAttribute('id', 'dg_sidebar');
+            this.wrapper.style['position'] = 'absolute';
+            this.wrapper.style['top'] = '160px';
+            this.wrapper.style['right'] = '0';
+            this.div.style['height'] = '100%';
+            this.div.style['width'] = '100%';
+            this.div.style['padding-left'] = this.ToggleButton.width * 1.5;
+            this.div.style['background'] = 'blue'; // TODO: remove this.
             this.ToggleButton.setup();
-            this.div.appendChild(this.ToggleButton.div);
-            document.body.appendChild(this.div);
+            this.wrapper.appendChild(this.ToggleButton.div);
+            this.wrapper.appendChild(this.div);
+            document.body.appendChild(this.wrapper);
             this.adjust();
             var self = this;
             window.onresize = function() { self.adjust(); };
         },
         adjust: function() {
-            this.div.style['height'] = (window.innerHeight - Element.positionedOffset(this.div)[1]) + 'px';
-            this.div.style['width'] = ((window.innerWidth / 100) * 25) + 'px';
+            this.wrapper.style['height'] = (window.innerHeight - Element.positionedOffset(this.wrapper)[1]) + 'px';
+            if(this.visible())
+                this.wrapper.style['width'] = ((window.innerWidth / 100) * 25) + 'px';
+            else
+                this.wrapper.style['width'] = this.ToggleButton.width + 'px';
         },
         insert: function() {
             for(i in arguments) {
@@ -39,17 +48,12 @@ var DigitalGazette = {
             }
         },
         toggle: function() {
-            if(this.div.style['right'] == '0px')
-                this.hide();
-            else
-                this.show();
+            this.div.toggle();
+            this.adjust();
         },
-        show: function() {
-            this.div.style['right'] = '0';
-        },
-        hide: function() {
-            this.div.style['right'] = -1 * (this.div.getWidth() - this.ToggleButton.width) + 'px';
-        },
+        visible: function() {
+            return (this.div.style['display'] != 'none');
+        }
     }
 };
 
