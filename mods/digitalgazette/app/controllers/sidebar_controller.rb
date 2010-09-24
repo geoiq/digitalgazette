82e1d94dfde7698a47aa8cf6cf1061ce51463174
niklas <niklas@brueckenschlaeger.de>
2010-09-24 22:56:47 +0200

    showing wikis in sidebar

--- mods/digitalgazette/app/controllers/sidebar_controller.rb
+++ mods/digitalgazette/app/controllers/sidebar_controller.rb
@@ -5,4 +5,11 @@ class SidebarController < ApplicationController
       page << 'DigitalGazette.Sidebar.adjust();'
     end
   end
+
+  def page
+    type = params[:type]
+    id = params[:id]
+    @page = MODEL_NAMES[type].constantize.find(id)
+    render :partial => "/sidebar/page/#{type}", :layout => false
+  end
 end
